ddfbe559c2033f82d4584425c5159a6e09076486
SHREYANCE1 <shreyancejain94@gmail.com>
2019-05-18 23:12:13 +0530

    p1-m3

---
+++ array-rotations/p1-m3-array-rotation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int gcd(int a, int b) {
+    if (a % b == 0){
+        return b;
+    }
+    return gcd(b, a%b);
+}
+
+void leftRotate(int arr[], int d, int n){
+    int split = gcd(n, d);
+    if (split == 1) {
+
+    } else {
+        for(int i = 0; i < split; i++){
+        }
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int d = 3;
+    leftRotate(arr, d, n);
+}
