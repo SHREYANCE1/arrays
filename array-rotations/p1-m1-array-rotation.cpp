b9b01c58c70e9f0634817022775967e9defa8cc3
SHREYANCE1 <shreyancejain94@gmail.com>
2019-05-18 22:05:15 +0530

    p1-m1

---
+++ array-rotations/p1-m1-array-rotation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void rotate(int arr[], int d, int n){
+    int temp[d];
+    // rotate by d = 2
+    // make a temp array of d elements
+    // int arr[] = {1,2,3,4,5,6,7};
+    // int temp[] = {1,2}
+    for(int i = 0; i < d; i++) {
+        temp[i] = arr[i];
+    }
+    // shift the remaining array
+    for(int i = 0; i < n-d; i++){
+        arr[i] = arr[i+d];
+    }
+    // resulting array = {3, 4, 5, 6, 7, 6, 7}
+    // replace the temp array at the end d elements
+    for(int i = 0; i < d; i++){
+       arr[n-(d-i)] = temp[i];
+    }
+    printArray(arr, n);
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int d = 2;
+    printArray(arr, n);
+    rotate(arr, d, n);;
+}
