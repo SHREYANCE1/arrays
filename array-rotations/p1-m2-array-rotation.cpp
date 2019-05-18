83caaf959e31cba1c3fef9c435f313e2b54c0fba
SHREYANCE1 <shreyancejain94@gmail.com>
2019-05-18 22:46:19 +0530

    p1-m2

--- array-rotations/p1-m2-array-rotation.cpp
+++ array-rotations/p1-m2-array-rotation.cpp
@@ -9,6 +9,29 @@ void printArray(int arr[], int n){
     cout << endl;
 }
 
-int main(){
-    
+// rotate array by one
+void rotateByOne(int arr[], int n){
+    int temp = arr[0], i;
+    for(i = 0; i <  n-1; i++){
+        arr[i] = arr[i+1];
+    }
+    arr[i] = temp;
 }
+
+void leftRotate(int arr[], int d, int n){
+    for(int i = 0; i < d; i++){
+        rotateByOne(arr, n);
+    }
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int d = 2;
+    printArray(arr, n);
+    leftRotate(arr, d, n);
+    printArray(arr, n);
+}
+
+// Time : O(n*d)
+// Space: O(1)
